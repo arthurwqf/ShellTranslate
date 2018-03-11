bd8fc8ca8ba8e8c0440e535a7f8b0d67ee54d143
贾楠楠 <jianannan@jianannandeMacBook-Air.local>
2018-03-11 20:02:27 +0800

    delete some files

--- Jiujiugk3/app/src/main/java/com/xingyun/jiujiugk/main/technology/presenter/TechnologyCenterPersenter.java
+++
@@ -1,9 +0,0 @@
-package com.xingyun.jiujiugk.main.technology.presenter;
-
-/**
- * Created by wangqf on 2017/2/23 0023.
- */
-
-public class TechnologyCenterPersenter {
-
-}
--- Jiujiugk3/app/src/main/java/com/xingyun/jiujiugk/main/technology/view/ITechnologyCenter.java
+++
@@ -1,9 +0,0 @@
-package com.xingyun.jiujiugk.main.technology.view;
-
-/**
- * Created by wangqf on 2017/2/23 0023.
- */
-
-public interface ITechnologyCenter {
-
-}
